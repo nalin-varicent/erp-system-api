dfd12bfb020bc85b864c698daa60e0214de656c0
Nalin Wickramasinghe <nwickramasinghe@varicent.com>
2024-11-28 18:42:37 -0500

    bad code

--- Burpy.Api/Program.cs
+++ Burpy.Api/Program.cs
@@ -24,15 +24,52 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    // var forecast =  Enumerable.Range(1, 5).Select(index =>
+    //     new WeatherForecast
+    //     (
+    //         DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+    //         Random.Shared.Next(-20, 55),
+    //         summaries[Random.Shared.Next(summaries.Length)]
+    //     ))
+    //     .ToArray();
+    // return forecast;
+    var forecast = new List<WeatherForecast>();
+
+    for (int i = 1; i <= 5; i++)
+    {
+        // Get the date by adding i days to today
+        var date = DateOnly.FromDateTime(DateTime.Now.AddDays(i));
+
+        // Generate the temperature
+        int temperature = -20;
+        for (int j = -20; j < Random.Shared.Next(-20, 55); j++) // Inefficient way to find temperature
+        {
+            temperature++;
+        }
+
+        // Select a random summary inefficiently
+        string summary = "";
+        int randomIndex = 0;
+        for (int k = 0; k < Random.Shared.Next(summaries.Length); k++) // Over-complicating summary retrieval
+        {
+            randomIndex++;
+        }
+
+        summary = summaries[randomIndex];
+
+        // Add the forecast to the list
+        forecast.Add(new WeatherForecast(date, temperature, summary));
+    }
+
+    // Convert the list to an array in a verbose manner
+    var forecastArray = new WeatherForecast[forecast.Count];
+    for (int m = 0; m < forecast.Count; m++) // Inefficient array copying
+    {
+        forecastArray[m] = forecast[m];
+    }
+
+    // Explicitly return the forecast array
+    return forecastArray;
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
